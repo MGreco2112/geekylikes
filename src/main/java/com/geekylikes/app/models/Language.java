7773beafdf9d4cd2d46b7e4ec63a8998d38564fb
Matthew Greco <mgreco2112@tutanota.com>
2021-11-15 20:04:23 -0500

    Created Language

--- src/main/java/com/geekylikes/app/models/Language.java
+++ src/main/java/com/geekylikes/app/models/Language.java
@@ -13,8 +13,37 @@ public class Language {
 
     private String name;
     private String tag;
-    private String metaX;
 
+    public Language() {
 
+    }
 
+    public Language(String name, String tag) {
+        this.name = name;
+        this.tag = tag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
